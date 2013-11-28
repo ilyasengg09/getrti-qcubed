7b2aa2e8e2c2db2ec7d183178dad90b0b8a39eb7
srvmdk <saurav@linuxb.in>
2013-11-28 10:57:39 +0530

    fixing urls

--- includes/views/templates/HeaderPanel.tpl.php
+++ includes/views/templates/HeaderPanel.tpl.php
@@ -6,11 +6,11 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/"><?php echo __SM_APP_NAME__; ?></a>
+			<a class="navbar-brand" href="<?php echo __SM_SITE_ADDRESS__; ?>"><?php echo __SM_APP_NAME__; ?></a>
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="/index.php">Home</a></li>
+				<li class="active"><a href="<?php echo __SM_SITE_ADDRESS__; ?>">Home</a></li>
 				<li><a href="<?php echo __SM_SITE_ADDRESS__.__SM_URL_REWRITE__; ?>/campaign/saverti">Save RTI</a></li>
 				<li><a href="<?php echo __SM_SITE_ADDRESS__.__SM_URL_REWRITE__; ?>/about">About</a></li>
 			</ul>
