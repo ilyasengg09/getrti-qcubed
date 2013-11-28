b74c5bb1abd2078cd84c3be41deb9f882edc1095
Harish Krishnan <bugsy.seigel@gmail.com>
2013-11-28 13:47:24 +0530

    new css changes

--- includes/views/MPDetailsView.tpl.php
+++ includes/views/MPDetailsView.tpl.php
@@ -1,33 +1,12 @@
 <div class="container">
-	<div class="row">
-		<div class="col-lg-12">
-			<div  style="background-color:lightgrey" class="row">
-				<div class="col-lg-8 col-lg-offset-2">
-					<div class="form-horizontal">
-						<div id="fg2" class="form-group">
 
-							<?php $_CONTROL->txtSearch->Render(); ?>
-							<?php $_CONTROL->btnGo->Render(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div id="fg2" class="row">
-				<div class="col-lg-12">
-					<h2>	<?php echo $_CONTROL->campaign->Description; ?>
-					</h2>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
+	<div style="margin-top:20px;" class="row">
 		<div class="col-lg-12">
 			<div style="background-color: antiquewhite;" class="row">
 				<div class="col-lg-4">
 					<div class="panel panel-primary">
 						<div class="panel-heading">
-							<h2 class="panel-title">
-	<?php $_CONTROL->lblMpName->Render(); ?></h2>
+	<?php $_CONTROL->lblMpName->Render(); ?>
 						</div>
 						<div class="panel-body">
 						
@@ -52,39 +31,31 @@
 							<div class="row">
 								<div class="panel panel-default">
 									<div class="panel-heading">
-										<h3 class="panel-title">MP's Stand on <?php echo $_CONTROL->strCampaign; ?></h3>
+										<h3 style="
+    margin-top: 20px;
+    margin-bottom: 10px;
+    font-size: 24px;" class="panel-title">MP's Stand on <?php echo $_CONTROL->strCampaign; ?></h3>
 									</div>
-									<div class="panel-body">
+									<div style="
+    min-height: 254px;" class="panel-body">
 										<?php $_CONTROL->lblMpStand->Render(); ?>
 									</div>
 								</div>
 							</div>
-							<div class="row">
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h3 class="panel-title">Your stand on <?php echo $_CONTROL->strCampaign; ?></h3>
-									</div>
-									<div class="panel-body">
-										<?php $_CONTROL->lblUserStand->Render(); ?>
-										<?php echo $_CONTROL->strVoteNow; ?>
-										<?php $_CONTROL->btnVoteFor->Render(); ?>
-										<?php $_CONTROL->btnVoteAgainst->Render(); ?>
-									</div>
-								</div>
-							</div>
+	
 						</div>
 						<div class="col-lg-6">
 							<div class="panel panel-success">
 								<div class="panel-heading">
-									<h3 class="panel-title">Constituency Stats</h3>
+									<h3 style="
+    font-size: 24px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+"  class="panel-title">Constituency Stats</h3>
 								</div>
-								<div class="panel-body">
+								<div style="min-height:250px;" class="panel-body">
 									<div class="col-lg-10 col-lg-offset-4">
-										<br />
-										<br />
 										<h1><?php $_CONTROL->lblConstituencyFor->Render(); ?></h1> <p class="text-success">FOR</p>
-										<br />
-										<br />
 										<h1><?php $_CONTROL->lblConstituencyAgainst->Render(); ?></h1> <p class="text-danger">AGAINST</p>
 									</div>
 								</div>
