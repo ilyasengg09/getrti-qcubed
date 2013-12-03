c5a68202b7be0d7ac8f81a50cbc6dd9b9f983eda
srvmdk <saurav@linuxb.in>
2013-12-03 18:05:16 +0530

    campaign page changes

--- includes/views/CampaignsView.tpl.php
+++ includes/views/CampaignsView.tpl.php
@@ -1,20 +1,13 @@
 <div class="container">
+	<div class="row" style="background-color: antiquewhite;">
+		<div class="col-lg-12">
+			<h2 style="font-weight:bold;padding-left:10px;"><?php echo $_CONTROL->campaign->Question; ?>
 
-	<div class="row" style="
-    background-color: antiquewhite;
-">
-		<div class="col-lg-12">
-			<h2 style="font-weight:bold;padding-left:10px;"><?php echo $_CONTROL->campaign->Description; ?>
 			</h2>
-		</div>
-	</div>
-	<?php include __VIEWS_PATH__.'/searchbox.tpl.php'; ?>
-	<div class="row">
-		<div class="col-lg-12">
-			<br />
 			<p><?php echo $_CONTROL->campaign->Longdescription; ?></p>
 		</div>
 	</div>
+	<?php include __VIEWS_PATH__.'/searchbox.tpl.php'; ?>
 	<div class="row">
 		<div class="col-lg-6">
 			<div class="panel panel-default">
