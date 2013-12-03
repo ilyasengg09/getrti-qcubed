f7f755cc39f00e9ac75e45698ce3c39c4c37a0f2
srvmdk <saurav@linuxb.in>
2013-12-03 17:17:02 +0530

    update question sql

---
+++ sql/update_question.sql
@@ -0,0 +1,1 @@
+UPDATE `campaigns` SET `description`="Do you think the political parties should come under the RTI Act?<br />Do you oppose the RTI Amendment Bill? Tell your MP what you think.<br />Find your MP below. Make your voice count!",`question`="Do you think the Parliament should pass the RTI Amendment Bill that keeps political parties out of the RTI Act?" WHERE `campaigns`.`id`=1
